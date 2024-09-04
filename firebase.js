09bc0c6865f376732e884b8e11ee7f758f5da5af
Amund Halgunset <amundhalgunset@gmail.com>
2024-09-04 12:43:10 +0200

    firebase

--- firebase.js
+++ firebase.js
@@ -1,15 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAIN4vtsDbgzu5wFugUrZJMTFx4iEL51wo",
-    authDomain: "tuvawebsite.firebaseapp.com",
-    projectId: "tuvawebsite",
-    storageBucket: "tuvawebsite.appspot.com",
-    messagingSenderId: "757289211872",
-    appId: "1:757289211872:web:80d6f06d1bcbcc07bbbd85",
-    measurementId: "G-1HZHZ0GMT6"
+    apiKey: "AIzaSyA4tjOXe0XiLJ4uaPrauiOo1BnEtUL4zgI",
+    authDomain: "luna-c56bc.firebaseapp.com",
+    projectId: "luna-c56bc",
+    storageBucket: "luna-c56bc.appspot.com",
+    messagingSenderId: "633279147738",
+    appId: "1:633279147738:web:10aab5e65a78b03304bfe3"
   };
 
-  firebase.initializeApp(firebaseConfig);
-
-  if (firebase.analytics) {
-    firebase.analytics();
-  }
+  const app = firebase.initializeApp(firebaseConfig);
